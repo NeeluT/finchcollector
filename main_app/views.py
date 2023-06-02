304d08bc0c4579145349883abed074d6c80fcb0f
NeeluT <nilufar.tavakoli@gmail.com>
2023-06-01 20:17:53 -0400

    Add feeding list and 'Add Feeding' feature

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
+
 
 
 
@@ -19,8 +21,9 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-    'finch': finch
+    'finch': finch, 'feeding_form': feeding_form
   })
 
 class FinchCreate(CreateView):
@@ -34,3 +37,14 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  #create a ModelForm instance using the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  #Validate the form
+  if form.is_valid():
+    # We want a model instance but we can't save to the db yet because we have not assigned the finch_id FK
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
